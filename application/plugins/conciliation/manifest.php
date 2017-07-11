746ba803e54ba2053b8fabd1a90ac6b3bc64c16e
thiagogol <goveiat@gmail.com>
2017-07-11 20:03:39 -0300

    alteração da versão

--- application/plugins/conciliation/manifest.php
+++ application/plugins/conciliation/manifest.php
@@ -2,6 +2,5 @@
 $plugin = array();
 $plugin['name'] = 'Conciliation';
 $plugin['author'] = 'Thiago Goveia';
-$plugin['version'] = '0.0.1';
+$plugin['version'] = '0.1.0';
 $plugin['description'] = 'Financial reconciliation Plugin for iBilling';
-$plugin['url'] = 'http://www.ibilling.io/plugin-development/';
