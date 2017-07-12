d23dd31a98a5354fba5a01262e310bc390e91c24
thiagogol <goveiat@gmail.com>
2017-07-12 10:24:05 -0300

    criação do i18n pt_br

---
+++ application/plugins/conciliation/i18n/pt_br.php
@@ -0,0 +1,28 @@
+<?php 
+	$_L_CONCIL[p_concil_bank_statement] = 'Extrato Bancário';
+	$_L_CONCIL[p_concil_upload_success] = 'Carregamento realizado com sucesso';
+	$_L_CONCIL[p_concil_conciliation] = 'Conciliação';
+	$_L_CONCIL[p_concil_req_ofx] = 'Faça o carregamento do extrato bancário no formato .OFX';
+	$_L_CONCIL[p_concil_invalid_account] = 'A conta deste extrato não está cadastrada';
+	$_L_CONCIL[p_concil_empty_ofx] = 'Faça o carregamento do arquivo .ofx';
+	$_L_CONCIL[p_concil_agency] ='Agência';
+	$_L_CONCIL[p_concil_account] ='Conta';
+	$_L_CONCIL[p_concil_bank] ='Banco';
+	$_L_CONCIL[p_concil_balance] ='Banco';
+	$_L_CONCIL[p_concil_start_date] ='Início';
+	$_L_CONCIL[p_concil_end_date] ='Fim';
+	$_L_CONCIL[p_concil_transactions] ='Transações';
+	$_L_CONCIL[p_concil_manual_t] ='Entradas Manuais';
+	$_L_CONCIL[p_concil_account_info] ='Informações da Conta';
+	$_L_CONCIL[p_concil_value] ='Valor';
+	$_L_CONCIL[p_concil_date] ='Data';
+	$_L_CONCIL[p_concil_id] ='Identificação';
+	$_L_CONCIL[p_concil_empty] ='Vazio';
+	$_L_CONCIL[p_concil_r_conciliated] ='registros conciliados';
+	$_L_CONCIL[p_concil_r_new] ='novos registros';
+	$_L_CONCIL[p_concil_r_exist] ='registros já existentes';
+	$_L_CONCIL[p_concil_invalid_ofx] ='O Arquivo .OFX não é válido';
+	$_L_CONCIL[p_concil_success_conciliate] ='Transações conciliadas com sucesso';
+	$_L_CONCIL[p_concil_empty_manual] ='Não há transações manuais referentes à data e à conta do extrato carregado';
+
+ ?>
